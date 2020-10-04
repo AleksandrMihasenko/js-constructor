220abfb5c6c6ca0cdc58b761075883b694b11e8d
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2020-10-04 16:00:18 +0300

    add parcel bundle with settings

---
+++ src/model.js
@@ -0,0 +1,10 @@
+export const model = [
+    { type: 'title', value: 'Hello from js!' },
+    { type: 'text', value: 'some text' },
+    { type: 'columns', value: [
+            'text 1',
+            'text 2',
+            'text 3'
+        ]},
+    {type: 'image', value: './images/js-image.png'}
+];
